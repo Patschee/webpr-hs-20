5d69db06c8c9dc1ceb95b641cf34fc3e352f894a
Patschee <patrick.schmid@students.fhnw.ch>
2020-09-22 15:23:58 +0200

    week1 changes and prep week2

---
+++ QUIZ/week2.js
@@ -0,0 +1,18 @@
+// const foo = () => true;
+// document.writeln(foo(1));
+//
+//
+// function two(x,y){
+//     return x+y;
+// }
+//
+// document.writeln(two(1,1,1))
+
+document.writeln("Woche2:")
+
+const plus = x => y => x + y
+
+document.writeln(plus(10))
+
+
+document.writeln()
